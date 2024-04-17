82b87fe4750474c92a8b51749238f2e39764c98f
Xvolved100 <Xvolved@hotmail.fr>
2024-04-17 16:20:16 +0200

    Ajout AboutUs+page

---
+++ src/Components/AboutUs/index.tsx
@@ -0,0 +1,70 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div className="lg:bg-fontDesktop bg-greenButton flex justify-center items-center">
+      <div className="max-w-3xl mx-auto p-8 lg:text-black text-white">
+        <h2 className="text-3xl font-bold mb-4">À Propos de Snap'n Go</h2>
+
+        <h3 className="text-xl font-bold mt-4">
+          Chapitre 1: L'Éclosion d'une Idée
+        </h3>
+        <p className="mt-4">
+          Il était une fois, dans un quartier animé d'une grande ville, quatre
+          amis inséparables : Emmanuel, Léa, Nicolas et Maxime. Tous
+          partageaient une passion commune pour une alimentation saine et
+          respectueuse de l'environnement.
+        </p>
+        <p className="mt-4">
+          Leur amitié était tissée de moments partagés à concocter des recettes
+          originales à base de produits bio, à arpenter les marchés locaux à la
+          recherche des meilleurs fruits et légumes de saison, et à discuter
+          sans fin de l'importance de consommer de manière responsable.
+        </p>
+
+        <h3 className="text-xl font-bold mt-4">
+          Chapitre 2: La Naissance de Snap'n Go
+        </h3>
+        <p className="mt-4">
+          Un jour, alors qu'ils sirotaient leur énième café en discutant de
+          leurs rêves et aspirations, l'idée germa dans leur esprit comme une
+          graine dans un sol fertile : pourquoi ne pas créer une épicerie en
+          ligne qui proposerait une sélection rigoureuse de produits bio et
+          équitables, tout en facilitant la vie des gens qui, comme eux,
+          cherchaient à bien manger sans sacrifier la praticité ?
+        </p>
+        <p className="mt-4">
+          C'est ainsi que naquit Snap'n Go. Emmanuel, Léa, Nicolas et Maxime
+          mirent toute leur énergie et leur passion dans ce projet, jonglant
+          entre leur emploi du temps chargé et les défis de la vie
+          d'entrepreneur.
+        </p>
+
+        <h3 className="text-xl font-bold mt-4">
+          Chapitre 3: La Quête des Fournisseurs
+        </h3>
+        <p className="mt-4">
+          La recherche des fournisseurs fut l'une des étapes les plus complexes.
+          Ils devaient trouver des producteurs locaux et des partenaires engagés
+          dans une démarche éthique, tout en garantissant des prix compétitifs
+          pour leurs clients.
+        </p>
+
+        <h3 className="text-xl font-bold mt-4">
+          Chapitre 4: Le Succès de Snap'n Go
+        </h3>
+        <p className="mt-4">
+          Aujourd'hui, Snap'n Go est devenue une référence dans le domaine de
+          l'alimentation bio en ligne. Emmanuel, Léa, Nicolas et Maxime
+          regardent avec fierté le chemin parcouru. Leur petite idée est devenue
+          une réalité florissante, et ils continuent d'inspirer de plus en plus
+          de gens à faire des choix alimentaires conscients et responsables, un
+          panier bio à la fois.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
---
+++ src/app/AboutUs/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AboutUs from "@/Components/AboutUs";
+import Footer from "@/Components/UI/footer";
+
+const App = () => {
+  return (
+    <div>
+        <AboutUs />
+        <Footer />
+    </div>
+  );
+};
+
+export default App;
